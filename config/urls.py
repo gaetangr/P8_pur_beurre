b66bddba6b16d764c22e4ed42443ffa014cdca8f
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-12-25 17:59:24 +0100

    Add homepage to project

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,10 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="pages/home.html"),
+        name="home",),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
 ]
