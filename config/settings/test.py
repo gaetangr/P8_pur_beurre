91248290c707fd5d370b38aac1cd102e97f3a89d
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-12-28 19:10:52 +0100

    add pytest

---
+++ config/settings/test.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "github_actions",
+        "USER": "radorado",
+        "PASSWORD": "radorado",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
--- purbeurre/users/forms.py
+++ purbeurre/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.forms import TextInput
+from django.forms import TextInput, PasswordInput
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
