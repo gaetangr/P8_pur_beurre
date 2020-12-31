6fc9916ce5cf4a3a51af044d2949e52cc37d4b49
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2020-12-31 08:54:54 +0100

    Updated yml config

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,10 +1,12 @@
+from .base import *
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "github_actions",
-        "USER": "radorado",
-        "PASSWORD": "radorado",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
+        "NAME": "",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
 }
