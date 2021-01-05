12a8f79015d706cfb2f549385a647b5ef5cb0a80
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-01-05 10:33:28 +0100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,11 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("*")
+ALLOWED_HOSTS = [
+    "https://purbeurre-py.herokuapp.com",
+    "http://purbeurre-py.herokuapp.com",
+    "purbeurre-py.herokuapp.com",
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
