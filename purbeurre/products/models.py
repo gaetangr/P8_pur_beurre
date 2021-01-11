7bf9ef507fcda53fafc22dc0b22f399317255b76
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-01-11 18:17:59 +0100

    clean up :sparkles:

--- purbeurre/products/models.py
+++ purbeurre/products/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-
-# models needed => Link image, img-nutrition-url -or- data,
-
 
 class Category(models.Model):
     """ Models for a category, related to a product"""
@@ -26,7 +22,6 @@ class Category(models.Model):
 class Product(models.Model):
     """ Models related to product """
 
-    # Required fields
     code = models.CharField("Code", max_length=200, unique=True)
 
     name = models.CharField(_("Name"), max_length=200)
