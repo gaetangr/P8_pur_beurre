fe7ab56e1ea1aead64a1e5dbbe4dc7074b477e0d
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-01-14 17:59:20 +0100

    Modify travis and chrome driver for functional testing

--- purbeurre/users/tests/test_users.py
+++ purbeurre/users/tests/test_users.py
@@ -5,19 +5,31 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 import time
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("window-size=1920x1080")
+
 
 class TestUserBehavior(StaticLiveServerTestCase):
     """Functional test for the login and registeration behiavor"""
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         """Setting up tests variables and config"""
-        self.browser = webdriver.Chrome("purbeurre/users/tests/chromedriver")
-        self.username = "Gaetan"
-        self.email = "hello@gaetangr.me"
-        self.password = "you-will-never-guess"
+        super().setUpClass()
+        cls.browser = webdriver.Chrome(
+            "purbeurre/users/tests/chromedriver", options=chrome_options
+        )
+        cls.username = "Gaetan"
+        cls.email = "hello@gaetangr.me"
+        cls.password = "you-will-never-guess"
+        cls.browser.implicitly_wait(10)
+        cls.browser.maximize_window()
 
-    def tearDown(self):
-        self.browser.close()
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.browser.quit()
 
     def test_if_user_can_register_and_login(self):
         """User should be able to register, login and then access their profil page"""
@@ -36,6 +48,7 @@ class TestUserBehavior(StaticLiveServerTestCase):
         self.browser.find_element_by_css_selector("#id_password2").send_keys(
             self.password
         )
+        time.sleep(3)
         self.browser.find_element_by_css_selector("#id_email").send_keys(self.email)
         self.browser.find_element_by_css_selector("#button-submit").click()
 
